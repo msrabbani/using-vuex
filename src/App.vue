97fece26cdba37548e79c18dacba3b2a8934faaa
msrabbani <msrabbanii@gmail.com>
2017-09-24 16:26:23 +0700

    without Vuex

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <product-list-one v-bind:products="products"/>
+    <product-list-two v-bind:products="products"/>
+  </div>
+</template>
+
+<script>
+import ProductListOne from './components/ProductListOne.vue'
+import ProductListTwo from './components/ProductListTwo.vue'
+
+export default {
+  components: {
+    'product-list-one': ProductListOne,
+    'product-list-two': ProductListTwo
+  },
+  name: 'app',
+  data () {
+    return {
+      products: [
+        {name: 'Kupling', price: 20},
+        {name: 'Bak Perseneling', price: 40},
+        {name: 'Busi', price: 60},
+        {name: 'CDI Vespa', price: 80},
+        {name: 'Lantai Vespa', price: 100}
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ProductListOne.vue
@@ -0,0 +1,49 @@
+<template lang="html">
+  <div id="product-list-one">
+    <h2>Product List One</h2>
+    <ul>
+      <li v-for ="productz in products">
+        <span class="name">{{productz.name}}</span>
+        <span class="price">Rp.{{productz.price}}</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['products'],
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="css">
+body{
+  font-family: Ubuntu;
+  color: #555;
+}
+
+#product-list-one{
+    background: #FFF8B1;
+    box-shadow: 1px 2px 3px rgba(0,0,0,0.2);
+    margin-bottom: 30px;
+    padding: 10px 20px;
+}
+#product-list-one ul{
+    padding: 0;
+}
+#product-list-one li{
+    display: inline-block;
+    margin-right: 10px;
+    margin-top: 10px;
+    padding: 20px;
+    background: rgba(255,255,255,0.7);
+}
+.price{
+    font-weight: bold;
+    color: #E8800C;
+}
+</style>
